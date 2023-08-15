d8895c6df4b7c4cc1a8dd46320036d4176b428cf
Gra-Vi-Ty <worakan223@gmail.com>
2023-08-15 20:33:08 +0700

    down

--- Homework/HW05/hw05-4-compare-number.cpp
+++ Homework/HW05/hw05-4-compare-number.cpp
@@ -25,3 +25,20 @@
     Output:
         Number1 equal Number2
 */
+#include <stdio.h>
+int main(){
+    int num1, num2 ;
+    printf("Input 1 :\n") ;
+    scanf("%d", &num1 ) ;
+    printf("Input 2 :\n") ;
+    scanf("%d", &num2 ) ;
+
+    if(num1 > num2 ){
+        printf("Number1 greater than Number2") ;
+    }else if(num1 < num2){
+        printf("Number1 lower than Number2") ;
+    }else if(num1 == num2){
+        printf("Number1 equal Number2") ;
+    }
+    return 0 ;
+}
